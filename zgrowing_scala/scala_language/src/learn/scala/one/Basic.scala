4d14e512e0e9995da9fd72caf6b89f76f584e69f
zhangdong <zhangdonginpeking@gmail.com>
2016-11-06 23:36:09 +0800

    add files

---
+++ zgrowing_scala/scala_language/src/learn/scala/one/Basic.scala
@@ -0,0 +1,15 @@
+package learn.scala.one
+
+class Basic {
+      
+  
+     //定义变量 ,  val 不可变 ， var 可变
+     var a = 1
+     val b = "msg"
+     
+     //具体类型可省略，有Scala类型推断
+     var c: Int = 90
+     val d: String = "zhangsan"
+     
+     var e = 4; var f = 10  //同一行的多条语句要有;分割，不同行时；可写可不写
+}
